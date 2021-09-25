420c301345edf3201d77e7cf033d2cbda6e7e017
Sebastian <rnsebastian701@gmail.com>
2021-09-24 22:54:05 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,10 @@
 <template>
   <v-app>
-    <!--
-    <v-navigation-drawer
-        v-model="sidebarMenu"
-        app
-        floating
-        :permanent="sidebarMenu"
-        :mini-variant.sync="mini"
-
-    >
-
-      <v-list-item class="px-2" @click="toggleMini = !toggleMini">
-
-        <v-list-item-content class="text-truncate">
-          Carol Skelly
-        </v-list-item-content>
-        <v-btn icon small>
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list v-for="item in items" :key="item.title"  class="text-center align-center" >
-
-        <v-btn color="purple" x-large class="white--text"  link :to="item.href">
-          {{item.title}}
-        </v-btn>
-
-      </v-list>
-      <v-btn plain to="/home" >
-        hola
-      </v-btn>
-
-    </v-navigation-drawer>
-    -->
+   
     <div class="container-app">
     <Sidebar></Sidebar>
     
-      <v-container>
+      <v-container class="content-view">
         <router-view/>
       </v-container>
 
@@ -59,5 +27,8 @@ export default {
 <style lang="scss">
   .container-app{
     display:flex;
+  }
+  .content-view{
+    overflow-y: auto !important;
   }
 </style>
